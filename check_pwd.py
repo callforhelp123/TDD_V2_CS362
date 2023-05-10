12209ba3a19530c2acf4739a6e2a147b624ee752
callforhelp123 <jared.a.chang@gmail.com>
2023-05-09 19:38:09 -0700

    test 0 lowercase

--- check_pwd.py
+++ check_pwd.py
@@ -1,12 +1,13 @@
 # PWD function
+# some code adapted from "Exploration: Random Testing"
 def check_pwd(input):
     if len(input) == 0:
         return False
-
     if len(input) < 8:
         return False
-
     if len(input) > 20:
+        return False
+    if not any(char.islower() for char in input):
         return False
 
     return True
